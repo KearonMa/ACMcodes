4bb449242122a1fad4c868ee31774b1f11709064
马凯欣 <Kearonma@163.com>
2017-07-17 17:51:37 +0800

    cf-gym

---
+++ gym-101343I.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    int m;
+    int ans[110];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+    {
+        scanf("%d",&m);
+        if(i==j)
+        {
+            ans[i]=sqrt(m);
+        }
+    }
+    for(int i=0;i<n;i++)
+        printf("%d ",ans[i]);
+    printf("\n");
+}
